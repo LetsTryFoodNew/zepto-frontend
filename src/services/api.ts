c70d49bc4ecbb567f43aa055f3ac93af8ccf89ae
Lets Try Tech Team <tech@letstryfoods.com>
2025-09-29 18:08:09 +0530

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 
 const api = axios.create({
-  baseURL: 'http://zepto-edi-integration-env-1.eba-wmmtjd4e.eu-north-1.elasticbeanstalk.com', // Change to your actual API base
+  // baseURL: 'http://zepto-edi-integration-env-1.eba-wmmtjd4e.eu-north-1.elasticbeanstalk.com', // Change to your actual API base
+  baseURL: 'https://edi.letstryfoods.com/', // Change to your actual API base
   headers: {
     'Content-Type': 'application/json',
   },
